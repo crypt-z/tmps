e5a24a79ffef50a0f5f4237226b62ec893edb62a
crypt-z <brewandbaker@gmail.com>
2018-02-21 01:42:33 +0300

    Update sendarr.php

--- sendarr.php
+++ sendarr.php
@@ -1,28 +1,34 @@
-<?php  error_reporting(E_ALL);
+<?php
+
+
+
+       error_reporting(E_ALL);
        ini_set('display_errors', 1);
-	     ini_set('display_startup_errors', 1);	   
-	     require_once 'SendRequest.php';
+       ini_set('display_startup_errors', 1);	   
+       require_once 'SendRequest.php';
      
-	   $test = (
+       $test = (
 		  new SendRequest ( $url ))
 		   -> headers ( $headers )
 		   -> post ([ 
        
-	   "Key"  => array("iPhone5", "iPhone5s", "iPhone6") ,
+       "Key"  => array("iPhone5", "iPhone5s", "iPhone6") ,
      
-     "Gen"  => array("iPhone5", "iPhone5s", "iPhone6") ,
+       "Gen"  => array("iPhone5", "iPhone5s", "iPhone6") ,
        
-	   "Byte" => array("iPhone5", "iPhone5s", "iPhone6") ,
+       "Byte" => array("iPhone5", "iPhone5s", "iPhone6") ,
      
-	   "apple"=> array("iPhone5", "iPhone5s", "iPhone6") ,
+       "apple"=> array("iPhone5", "iPhone5s", "iPhone6") ,
      
-	   "samsumg"=> array("Samsung Galaxy III", "Samsung Galaxy ACE II"),
+       "samsumg"=> array("Samsung Galaxy III", "Samsung Galaxy ACE II"),
+   
+       "nokia" => array("Nokia N9", "Nokia Lumia 930"),
      
-	   "nokia" => array("Nokia N9", "Nokia Lumia 930"),
-     
-	   "sony" => array("Sony XPeria Z3", "Xperia Z3 Dual", "Xperia T2 Ultra"),
+       "sony" => array("Sony XPeria Z3", "Xperia Z3 Dual", "Xperia T2 Ultra"),
      
 	  ]) ;
     
 		 print_r ($test).'<br>' ;
- ?>
+ 
+
+?>
