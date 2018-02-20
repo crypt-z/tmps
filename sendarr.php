cbdf6f74a0963867b59e9374f9675779a37749ec
crypt-z <brewandbaker@gmail.com>
2018-02-21 02:28:37 +0300

    Update sendarr.php

--- sendarr.php
+++ sendarr.php
@@ -1,6 +1,6 @@
 <?php
 
-     require_once'SendRequest.php';
+     require_once'Config.php';
 error_reporting(E_ALL);
         ini_set( 'display_errors', 1);
         ini_set( 'display_startup_errors', 1);	    
