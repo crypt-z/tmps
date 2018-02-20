882b76009079640f48e36ea4d1e39e1e358488c3
crypt-z <brewandbaker@gmail.com>
2018-02-21 02:22:14 +0300

    Update sendarr.php

--- sendarr.php
+++ sendarr.php
@@ -3,7 +3,7 @@
   error_reporting(E_ALL);
           ini_set( 'display_errors', 1);
           ini_set( 'display_startup_errors', 1);	   
- require_once 'SendRequest.php';
+ require_once 'RequestConfig.php';
          $This = (
  new SendRequest ( $url ) )
     ->  headers  ( $headers )
